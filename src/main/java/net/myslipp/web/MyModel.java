6c5b6f43921f79147d8f670aa0b3fea332a48d75
jehyeon <s85737@gmail.com>
2021-01-31 13:14:17 +0900

    html template 추가

---
+++ src/main/java/net/myslipp/web/MyModel.java
@@ -0,0 +1,14 @@
+package net.myslipp.web;
+
+public class MyModel {
+    private String name;
+
+    public MyModel(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
--- src/main/java/net/myslipp/web/WelcomeController.java
+++ src/main/java/net/myslipp/web/WelcomeController.java
@@ -5,14 +5,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class WelcomeController {
 
     @GetMapping("/hello")
     public String welcome(String name, int age, Model model){
-        System.out.println("name : " + name);
-        model.addAttribute("name", name);
-        model.addAttribute("age", age);
+        List<MyModel> repo = Arrays.asList(new MyModel("jaehyeon"), new MyModel("javajigi"));
+        model.addAttribute("repo", repo);
+        //        System.out.println("name : " + name);
+//        model.addAttribute("name", name);
+//        model.addAttribute("age", age);
         return "welcome";
     }
 
