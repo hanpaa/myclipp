d4bbaefc25facd6747b54be3e64089480ebc4203
jehyeon <s85737@gmail.com>
2021-02-06 17:32:51 +0900

    make header, footer

--- src/main/java/net/myslipp/web/UserController.java
+++ src/main/java/net/myslipp/web/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -38,6 +39,13 @@ public class UserController {
     @GetMapping("/form")
     public String form(){
         return "/user/form";
+    }
+
+    @GetMapping("{userId}/form")
+    public String updateForm(@PathVariable Long userId, Model model){
+        User user = userRepository.findById(userId).get();
+        model.addAttribute("user", user);
+        return "/user/updateForm";
     }
 
     @GetMapping("/login")
