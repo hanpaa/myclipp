67adba6f529295656ffe4b42d45d0e9a086d04f9
jehyeon <s85737@gmail.com>
2021-02-07 18:53:54 +0900

    post방식을 put방식으로 바꿔보기

--- src/main/java/net/myslipp/web/UserController.java
+++ src/main/java/net/myslipp/web/UserController.java
@@ -6,10 +6,7 @@ import net.myslipp.domain.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -48,7 +45,9 @@ public class UserController {
         return "/user/updateForm";
     }
 
-    @PostMapping("/{id}")
+    //html은 get와 post만 지원하지만, put쓰는게 좋긴함
+
+    @PutMapping("/{id}")
     public String update(@PathVariable Long id, User updateUser){
         User user = userRepository.findById(id).get();
         user.update(updateUser);
@@ -65,5 +64,6 @@ public class UserController {
     public String profile(){
         return "/user/profile";
     }
+
 
 }
